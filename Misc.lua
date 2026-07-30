899dd55a81f90d86f53ed71f48d00148e4fe4883
Selunar <156382268+Selunariorium@users.noreply.github.com>
2026-07-31 02:57:21 +0700

    Update state management in Misc.Glue function
    Refactor glue timeout handling and state management.

--- Misc.lua
+++ Misc.lua
@@ -273,6 +273,8 @@ end
 ----------------------------------------------------------------
 
 -- useShiftlock is useless right now 
+state.start = nil
+state.Timeout = 10
 function Misc.Glue(Root, Offset, Toggle, UseDesync, useShiftlock, Yield, Timeout)
 	state.glueActive = false
 	state.Yield = false
@@ -302,12 +304,12 @@ function Misc.Glue(Root, Offset, Toggle, UseDesync, useShiftlock, Yield, Timeout
 
 	if not Toggle then return end
 	
-	Timeout = typeof(Timeout) ~= 'number' and 10 or Timeout
-	local start = os.clock()
+	state.Timeout = typeof(Timeout) ~= 'number' and 10 or Timeout
+	state.start = os.clock()
 	
 	task.spawn(function()
 		repeat RunService.RenderStepped:Wait()
-			if os.clock() - start >= Timeout then
+			if root and root.Parent and root.Parent.Humanoid and root.Parent.Humanoid.Health > 0 and os.clock() - state.start >= state.Timeout then
 				Misc.StopGlue()
 				break
 			end
